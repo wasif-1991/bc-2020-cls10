7318cede5b39511547b38b26e783f4e0c4161870
wasif-1991 <67942918+wasif-1991@users.noreply.github.com>
2020-11-26 01:14:53 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
+
+import image from './images/image.png';
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+
+        <section>
+          <hr/>
+          <footer>Designed and Developed by <a rel="noreferrer" href="https://upshotfirm.com" target="_blank" className={styles.footer}>Wasif Ahmed</a>.</footer>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
